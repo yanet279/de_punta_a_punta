242f10e673b348fab97e7cbdbf0e198fc1a89c28
Yanet Soledad Choque Cayo <sol3127g@gmail.com>
2023-10-21 16:03:13 -0300

    Implemente un carousel en index.html. Edite su estilo según el ancho de la pantalla y le di funcionalidades al carousel con js.

--- js/index.js
+++ js/index.js
@@ -9,3 +9,22 @@ menuHamburguesa.addEventListener('click', e => {
     const inferior = document.querySelector('.hamburguesa-button__inferior') 
     inferior.classList.toggle('hamburguesa-button__inferior--close')
 })
+
+/////////////////////   CAROUSEL   /////////////////
+
+
+function checkScreenWidth (){
+    let carousel = document.querySelector("#carouselExampleIndicators")
+    let imagenes= document.querySelectorAll(".imagenes")
+
+    const anchoPantalla = window.innerWidth;
+    if(anchoPantalla >= 910) {
+        carousel.classList.remove("carousel","slide")
+        imagenes.forEach(imagenes => imagenes.classList.remove("carousel-item"))
+    }else {
+        carousel.classList.add("carousel","slide")
+        imagenes.forEach(imagenes => imagenes.classList.add("carousel-item"))
+    }
+}
+window.addEventListener("load", checkScreenWidth);
+window.addEventListener("resize", checkScreenWidth);
